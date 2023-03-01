4cabc7e972de636825d66869f39b1fb8be5eec10
NicolasRojas1 <nicrojasc6@gmail.com>
2023-02-28 22:10:13 -0500

    [ADD] registro animacion

---
+++ js/registro.js
@@ -0,0 +1,22 @@
+/**Por medio de AJAX cambio la pagina */
+$(function() {
+  $("#tarjetaRegistro").load("registrarse.html");
+}); 
+
+function abrirTarjetaLogin() {
+  document.getElementById("tarjeta-registro").classList.add("inner");
+  const pause = (time) => new Promise(resolve => setTimeout(resolve,time))
+  async function start() {
+    await pause(1110)
+    navegarALogin()
+  }
+  start()
+
+}
+
+function navegarALogin() {
+
+  $(function () {
+    $("#tarjetaRegistro").load("iniciar-sesion.html");
+  });
+}
