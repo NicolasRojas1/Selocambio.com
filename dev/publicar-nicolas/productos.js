02a80f549bc9d1aa99b0bec05ef24aed23649616
NicolasRojas1 <nicrojasc6@gmail.com>
2023-03-04 23:00:40 -0500

    [add] publicar-nicolas archivo

---
+++ dev/publicar-nicolas/productos.js
@@ -0,0 +1,92 @@
+const products = [
+  {
+    id: 1,
+    nombre: "laptop",
+    img: "https://http2.mlstatic.com/D_NQ_NP_825200-MCO48213888172_112021-O.webp",
+    descripcion: "Ofrezco computadora con uso de 1 año",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 2,
+    nombre: "televisor lcd",
+    img: "https://www.lg.com/co/images/televisores/md07534007/D-01.jpg",
+    descripcion: "Ofrezco televisor marca lg uso de 2 años",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 3,
+    nombre: "xbox series x",
+    img: "https://exitocol.vtexassets.com/arquivos/ids/7234185-1200-auto?v=637532554324870000&width=1200&height=auto&aspect=true",
+    descripcion: "Ofrezco xbox series x con 6 meses de uso",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 4,
+    nombre: "playstation 5",
+    img: "https://i.linio.com/p/eacdaceecd78e05b6d595e5d827fef26-product.webp",
+    descripcion: "Ofrezco playstation 5 con 2 meses de uso",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 5,
+    nombre: "laptop",
+    img: "https://http2.mlstatic.com/D_NQ_NP_825200-MCO48213888172_112021-O.webp",
+    descripcion: "Ofrezco computadora con uso de 1 año",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 6,
+    nombre: "televisor lcd",
+    img: "https://www.lg.com/co/images/televisores/md07534007/D-01.jpg",
+    descripcion: "Ofrezco televisor marca lg uso de 2 años",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 7,
+    nombre: "xbox series x",
+    img: "https://exitocol.vtexassets.com/arquivos/ids/7234185-1200-auto?v=637532554324870000&width=1200&height=auto&aspect=true",
+    descripcion: "Ofrezco xbox series x con 6 meses de uso",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 8,
+    nombre: "playstation 5",
+    img: "https://i.linio.com/p/eacdaceecd78e05b6d595e5d827fef26-product.webp",
+    descripcion: "Ofrezco playstation 5 con 2 meses de uso",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 9,
+    nombre: "laptop",
+    img: "https://http2.mlstatic.com/D_NQ_NP_825200-MCO48213888172_112021-O.webp",
+    descripcion: "Ofrezco computadora con uso de 1 año",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+  {
+    id: 10,
+    nombre: "televisor lcd",
+    img: "https://www.lg.com/co/images/televisores/md07534007/D-01.jpg",
+    descripcion: "Ofrezco televisor marca lg uso de 2 años",
+    vendedor: "Pepito Perez",
+    telefono: 3002001020,
+    email: "pepito@gmail.com"
+  },
+];
