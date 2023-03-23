d42e308a151846d78ef4960fca70635e6df952cb
Adolfo Sánchez Lopéz <sanchezlopezadolfo@gmail.com>
2023-03-23 11:35:33 -0500

    [fix] ajustes usabilidad

--- html/inicio/app.js
+++ html/inicio/app.js
@@ -9,10 +9,18 @@ products.forEach((products) => {
   content.className = 'card';
 
   content.innerHTML = `
-  <img src="${products.img}"></img>
+  <style>
+  img.img_prod" {
+    min-height: 200px;
+    min-weight: 200px;
+    object-fit: cover;
+  }
+
+  </style>
+  <img class="img_prod" src="${products.img}"></img>
   <h2>${products.nombre}</h2>
   <h4>${products.vendedor}</h4>
-  <p>${products.descripcion}</p>
+  <p class="description">${products.descripcion}</p>
   <a href="http://wa.me/57${products.telefono}?text=Hola ${products.vendedor}, vi tu oferta de ${products.nombre} en la pagina de Selocambio.com. ¿En qué estás interesado para poder hacerte el cambio?" target="_blank"> Selocambio
   `;
 
